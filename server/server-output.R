36f3b2c23271f0db24825851c7f6f375c25fbc6e
Garrett Dancik <garrett.dancik@gmail.com>
2018-01-14 10:51:04 -0500

    Update datasets

--- server/server-output.R
+++ server/server-output.R
@@ -62,7 +62,7 @@ output$GeneColumn <- renderUI({
 # dynamically change shinyTitle
 #############################################
 
-shinyTitle = "shinyGEO <span style ='font-size:60%;'>(last updated: 12/12/17)</span>"
+shinyTitle = "shinyGEO <span style ='font-size:60%;'>(last updated: 01/14/18)</span>"
 
 output$shinyTitle = renderText(shinyTitle)
 
